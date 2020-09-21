af0c8d99d414b3f2071db818b2f14047aa3f10a9
Carol Susie Odiorne <carolsusieo@users.noreply.github.com>
2020-09-21 17:58:15 -0500

    Create odiorne.config.js

---
+++ odiorne.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+apps : [
+     {
+       name          : 'odiorne',
+       script        : 'npx',
+       interpreter   : 'none',
+       args          : 'serve build -s',
+       env_production : {
+         NODE_ENV: 'production',
+         NODE_PORT: '8003'
+       }
+     }
+   ]
+}module.exports = {
+apps : [
+     {
+       name          : 'web4u4less',
+       script        : 'npx',
+       interpreter   : 'none',
+       args          : 'serve build -s',
+       env_production : {
+         NODE_ENV: 'production',
+         NODE_PORT: '8002'
+       }
+     }
+   ]
+}
